7c483841a81a39d16839fb18bd0898b0bcd1f078
Joshua Ledbetter <ledbetterljoshua@gmail.com>
2014-11-29 12:11:03 -0600

    changed production file to match current URL

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,7 +80,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-  config.action_mailer.default_url_options = { :host => 'http://secret-mesa-7549.herokuapp.com/' }
+  config.action_mailer.default_url_options = { :host => 'https://workhorsebar.herokuapp.com/' }
   config.paperclip_defaults = {
   :storage => :s3,
   :s3_credentials => {
